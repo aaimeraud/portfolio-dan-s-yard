4cb0124068fd89ffeb125667cc11cff5d874b205
aaimeraud <danielfortune.pro@gmail.com>
2025-11-15 19:58:49 +0100

    Removed Script.js

--- src/script.js
+++
@@ -1,9 +0,0 @@
-import Typed from 'typed.js';
-
-var typed = new Typed("#header", {
-        strings: ["learn about me.", "hire me?"],
-        typeSpeed: 50,
-        backSpeed: 25,
-        showCursor: false,
-        loop: true,
-      });
